3d72e5d03c1a245fa9db5017da89945281aef081
asadulloev <asadulloev@deck.lc>
2024-07-09 15:56:16 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,121 @@
+import viteLogo from '/vite.svg';
+import { CounterId, DecrementAction, IncrementAction, store, AppState } from './store';
+import { useEffect, useReducer, useRef } from 'react';
+
+function App() {
+  return (
+    <>
+      <div>
+        <a
+          href="https://vitejs.dev"
+          target="_blank">
+          <img
+            src={viteLogo}
+            className="logo"
+            alt="Vite logo"
+          />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <Counter counterId="first" />
+      <Counter counterId="second" />
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+// export function Counter({ counterId }: { counterId: CounterId }) {
+//   const [, forceUpdate] = useReducer((x) => x + 1, 0);
+
+//   useEffect(() => {
+//     const unsubscribe = store.subscribe(() => {
+//       forceUpdate();
+//     });
+
+//     return unsubscribe;
+//   }, []);
+//   return (
+//     <>
+//       counter {store.getState().counters[counterId]?.counter}
+//       <button
+//         onClick={() =>
+//           store.dispatch({
+//             type: 'increment',
+//             payload: { counterId },
+//           } satisfies IncrementAction)
+//         }>
+//         increment
+//       </button>
+//       <button
+//         onClick={() =>
+//           store.dispatch({
+//             type: 'decrement',
+//             payload: { counterId },
+//           } satisfies DecrementAction)
+//         }>
+//         decriment
+//       </button>
+//     </>
+//   );
+// }
+
+ const selectCounter = (state: AppState, counterId: CounterId) =>
+  state.counters[counterId];
+
+export function Counter({ counterId }: { counterId: CounterId }) {
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+  console.log("render counter", counterId);
+
+  const lastStateRef = useRef<ReturnType<typeof selectCounter>>();
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      const currentState = selectCounter(store.getState(), counterId);
+      const lastState = lastStateRef.current;
+
+      if (currentState !== lastState) {
+        forceUpdate();
+      }
+
+      lastStateRef.current = currentState;
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const counterState = selectCounter(store.getState(), counterId);
+  return (
+    <>
+      counter {counterState?.counter}
+      <button
+        onClick={() =>
+          store.dispatch({
+            type: "increment",
+            payload: { counterId },
+          } satisfies IncrementAction)
+        }
+      >
+        increment
+      </button>
+      <button
+        onClick={() =>
+          store.dispatch({
+            type: "decrement",
+            payload: { counterId },
+          } satisfies DecrementAction)
+        }
+      >
+        decriment
+      </button>
+    </>
+  );
+}
+
+export default App;
